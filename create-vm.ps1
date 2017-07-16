682e26c9f7715f15d7d27b00ad8d90b53c83a237
Tyler <andruschak@gmail.com>
2017-07-16 01:41:35 -0600

    customized edition

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,46 +1,70 @@
 # Variables for common values
 # from - https://docs.microsoft.com/en-us/azure/virtual-machines/scripts/virtual-machines-linux-powershell-sample-create-vm?toc=%2fpowershell%2fmodule%2ftoc.json
+#
+# before running execute: "Login-AzureRmAccount" and enter your azure user/pass
+#
+# this will lump the vm-nic, vm-nsg, etc to the resource group - if you want to delete, turf the resource group
+
+
+
+
 
 $resourceGroup = "vm-lab"
 $location = "westus"
-$vmName = "vm-lab-server1"
+$vmName = "az-lab-server1"
 
 # Definer user name and blank password
 $securePassword = ConvertTo-SecureString ' ' -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
 
+
+
+
 # Create a resource group
 New-AzureRmResourceGroup -Name $resourceGroup -Location $location
 
+
+
+
 # Create a subnet configuration
-$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name my-lab-Subnet -AddressPrefix 192.168.1.0/24
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name az-lab-Subnet -AddressPrefix 10.12.1.0/24
 
 # Create a virtual network
 $vnet = New-AzureRmVirtualNetwork -ResourceGroupName $resourceGroup -Location $location `
-  -Name vm-lab-vNET -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+  -Name az-lab-vNET -AddressPrefix 10.12.0.0/16 -Subnet $subnetConfig
 
-# Create a public IP address and specify a DNS name
+# Create a public IP address and specify a DNS name, was "mypublicdns$(Get-Random)" for random name
 $pip = New-AzureRmPublicIpAddress -ResourceGroupName $resourceGroup -Location $location `
-  -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+  -Name "az-lab-publicdns" -AllocationMethod Static -IdleTimeoutInMinutes 4
 
 # Create an inbound network security group rule for port 22
-$nsgRuleSSH = New-AzureRmNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleSSH  -Protocol Tcp `
+$nsgRuleSSH = New-AzureRmNetworkSecurityRuleConfig -Name az-lab-NetworkSecurityGroupRuleSSH  -Protocol Tcp `
   -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
   -DestinationPortRange 22 -Access Allow
 
 # Create a network security group
 $nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location `
-  -Name myNetworkSecurityGroup -SecurityRules $nsgRuleSSH
+  -Name az-lab-NetworkSecurityGroup -SecurityRules $nsgRuleSSH
 
 # Create a virtual network card and associate with public IP address and NSG
-$nic = New-AzureRmNetworkInterface -Name myNic -ResourceGroupName $resourceGroup -Location $location `
+$nic = New-AzureRmNetworkInterface -Name az-lab-Nic -ResourceGroupName $resourceGroup -Location $location `
   -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+
+
+
+# Actual vm piece
+# Was -VMSize Standard_D1, but we want to use the free tier
 
 # Create a virtual machine configuration
 $vmConfig = New-AzureRmVMConfig -VMName $vmName -VMSize Standard_D1 | `
 Set-AzureRmVMOperatingSystem -Linux -ComputerName $vmName -Credential $cred -DisablePasswordAuthentication | `
 Set-AzureRmVMSourceImage -PublisherName Canonical -Offer UbuntuServer -Skus 14.04.2-LTS -Version latest | `
 Add-AzureRmVMNetworkInterface -Id $nic.Id
+
+
+# Need to configure youre ssh keys
+# after choco install putty you can use puttygen to generate keys or git bash with 
 
 # Configure SSH Keys
 $sshPublicKey = Get-Content "$env:USERPROFILE\.ssh\id_rsa.pub"
